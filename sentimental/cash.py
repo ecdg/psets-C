b571f6f64bb3a105f1f77e6e584c5ef09b010fdc
Emily Dayanghirang <emilydayang@gmail.com>
2019-07-27 23:29:49 -0700

    Add program cash.py

---
+++ sentimental/cash.py
@@ -0,0 +1,28 @@
+# Implementation of Greedy algorithm
+# User inputs how much change is owed
+# Program prints the minimum number of coins w/ which that change can be made
+
+from cs50 import get_int, get_float
+
+
+def main():
+    # Prompts user for valid input of amount of change owed
+    while True:
+        dollars_owed = get_float("Change owed: ")
+        cents_owed = round(dollars_owed * 100)
+        if cents_owed > 0:
+            break
+
+    # Finding the best immediate change
+    quarters = cents_owed // 25
+    dimes = (cents_owed % 25) // 10
+    nickels = ((cents_owed % 25) % 10) // 5
+    pennies = ((cents_owed % 25) % 10) % 5
+
+    # Prints minimum number of coins
+    print(quarters + dimes + nickels + pennies)
+
+
+# Executes main function
+if __name__ == "__main__":
+    main()
